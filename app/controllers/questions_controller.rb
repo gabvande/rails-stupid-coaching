320048c29c0ab0d84e25a18b2caab041208225d5
gabi van den brule <gvandenbrule@gmail.com>
2019-11-04 15:05:49 +0100

    styles are no so good

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+
+    if @question == 'I am going to work'
+      @reply = 'Great!'
+    elsif @question[-1] == '?'
+      @reply = 'Silly question, get dressed and go to work!'
+    else
+      @reply = 'I do not care, get dressed and go to work!'
+    end
+  end
+end
